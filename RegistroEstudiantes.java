b4b41e3d927e53a6f30f082aa6142a7e8bb4fd29
Jose Sierra <josemxras1000@gmail.com>
2026-08-30 21:14:44 -0500

    agregar modificar estuidante y busqueda

--- RegistroEstudiantes.java
+++ RegistroEstudiantes.java
@@ -45,5 +45,45 @@ public class RegistroEstudiantes {
 
         return null;
     }
+    // Buscar por autonumerico 
+    //sirve para identificar temporalmente la posicion de cada estudiante dentro del vector y facilitar operaciones como buscar, modificar y eliminar
+    public Estudiante buscarPorAutonumerico(int numero) {
+
+        if (numero < 1 || numero > cantidad) {
+            return null;
+        }
+
+        return estudiantes[numero - 1];
+    }
+
+    // Modificar estudiante
+    public boolean modificar(int numero, Estudiante nuevoEstudiante) {
+
+        if (numero < 1 || numero > cantidad) {
+            return false;
+        }
+
+        // Verificar que la nueva cedula no pertenezca a otro estudiante
+        for (int i = 0; i < cantidad; i++) {
+
+            if (i != numero - 1 &&
+                estudiantes[i].getCedula().equals(nuevoEstudiante.getCedula())) {
+
+                return false;
+            }
+        }
+
+        // Guarda las notas anteriores
+        double[] notasAnteriores = estudiantes[numero - 1].getNotas();
+
+        estudiantes[numero - 1] = nuevoEstudiante;
+
+        // Mantener las notas que ya tenia
+        for (int i = 0; i < notasAnteriores.length; i++) {
+            estudiantes[numero - 1].getNotas()[i] = notasAnteriores[i];
+        }
+
+        return true;
+    }
 
 }
