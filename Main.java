a1dced1aee9357a520e717d5201acee2eff0bee2
Jose Sierra <josemxras1000@gmail.com>
2026-08-30 21:19:51 -0500

    creacion de clase main scanner y objeto

---
+++ Main.java
@@ -0,0 +1,9 @@
+import java.time.LocalDate;
+import java.util.Scanner;
+
+public class Main{
+
+    static Scanner teclado = new Scanner(System.in);
+    static RegistroEstudiantes registro = new RegistroEstudiantes();
+
+}
