4eabcb7599c84025b52105719295383a6d69e3a9
Jose Sierra <josemxras1000@gmail.com>
2026-08-30 20:57:33 -0500

    implementacion de metodos para notas, edad y promedio

--- Estudiante.java
+++ Estudiante.java
@@ -61,5 +61,114 @@ public class Estudiante{
     public void setFechaNacimiento(LocalDate fechaNacimiento) {
         this.fechaNacimiento = fechaNacimiento;
     }
+    // Calcular edad
+    public int getEdad() {
+        return Period.between(fechaNacimiento, LocalDate.now()).getYears();
+    }
 
+    // Buscar una posición libre en el vector de notas
+    public int buscarPosicionLibre() {
+
+        for (int i = 0; i < notas.length; i++) {
+            if (notas[i] == 0) {
+                return i;
+            }
+        }
+
+        return -1;
+    }
+
+    // Agregar nota
+    public boolean agregarNota(double nota) {
+
+        int posicion = buscarPosicionLibre();
+
+        if (posicion == -1) {
+            return false;
+        }
+
+        notas[posicion] = nota;
+        return true;
+    }
+
+    // Modificar nota
+    public boolean modificarNota(int posicion, double nuevaNota) {
+
+        if (posicion < 0 || posicion >= notas.length) {
+            return false;
+        }
+
+        if (notas[posicion] == 0) {
+            return false;
+        }
+
+        notas[posicion] = nuevaNota;
+        return true;
+    }
+
+    // Eliminar nota
+    public boolean eliminarNota(int posicion) {
+
+        if (posicion < 0 || posicion >= notas.length) {
+            return false;
+        }
+
+        if (notas[posicion] == 0) {
+            return false;
+        }
+
+        // Mover las notas siguientes hacia atras
+        for (int i = posicion; i < notas.length - 1; i++) {
+            notas[i] = notas[i + 1];
+        }
+
+        notas[notas.length - 1] = 0;
+
+        return true;
+    }
+
+    // Cantidad de notas registradas
+    public int cantidadNotas() {
+
+        int cantidad = 0;
+
+        for (int i = 0; i < notas.length; i++) {
+            if (notas[i] != 0) {
+                cantidad++;
+            }
+        }
+
+        return cantidad;
+    }
+
+    // Calcular promedio del estudiante
+    public double calcularPromedio() {
+
+        double suma = 0;
+        int cantidad = 0;
+
+        for (int i = 0; i < notas.length; i++) {
+
+            if (notas[i] != 0) {
+                suma += notas[i];
+                cantidad++;
+            }
+        }
+
+        if (cantidad == 0) {
+            return 0;
+        }
+
+        return suma / cantidad;
+    }
+
+    // Mostrar datos
+    public void mostrarDatos() {
+
+        System.out.println("Cedula: " + cedula);
+        System.out.println("Nombres: " + nombres);
+        System.out.println("Apellidos: " + apellidos);
+        System.out.println("Fecha de nacimiento: " + fechaNacimiento);
+        System.out.println("Edad: " + getEdad());
+    }
 }
